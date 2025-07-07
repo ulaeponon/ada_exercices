54cc17885338bdff6a0fe3b33d9553ab37bf89c3
Ursula Eponon <ulaeponon@gmail.com>
2025-07-07 10:30:54 +0200

    add exo1

---
+++ exo1.js
@@ -0,0 +1,10 @@
+let number= -8
+
+if (number>0) { console.log("ce nombre est positif !")
+    
+} else if(number<0){ console.log("ce nombre est négatif!")
+    
+}
+else{console.log("ce nombre est nul !")
+
+};
