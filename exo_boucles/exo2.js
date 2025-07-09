24e29a81c59a2961fcd58b82a0c0d2fb6cf6ae99
Ursula Eponon <ulaeponon@gmail.com>
2025-07-09 14:54:44 +0200

    Suppression de exo1.js, exo2.js et exo 3.js

--- exo_boucles/exo2.js
+++
@@ -1,7 +0,0 @@
-// let letters = [ 'A', 'd', 'a', '-', 'T', 'e', 'c', 'h'];
-let letters = [ 'A', 'd', 'a', '-', 'S', 'c', 'h', 'o', 'o', 'l']
-let message;
-for (let index = 0; index < letters.length; index++) {
-    message+= letters[index];
-};
-console.log(message);
